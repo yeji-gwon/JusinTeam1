dc4c222111fa04fb8bb726607659c85a4d719d6e
yeonii01 <adencho1204@naver.com>
2025-05-27 00:11:14 +0900

    손가락 관절 구조, 렌더(수정중)
    손가락 관절 구조, 렌더(수정중)

---
+++ DefaultWindow/CFingers.cpp
@@ -0,0 +1,67 @@
+#include "pch.h"
+#include "CFingers.h"
+#include "CFinger.h"
+
+CFingers::CFingers()
+{
+}
+
+CFingers::~CFingers()
+{
+}
+
+void CFingers::Initialize()
+{
+	m_mapFingers["thumb"] = new CFinger;
+	m_mapFingers["thumb"]->Initialize();
+	dynamic_cast<CFinger*>(m_mapFingers["thumb"])->Set_Trans({ 400, 600 }, { 300, 510 }, { 285,450 }, { 285,420 });
+	dynamic_cast<CFinger*>(m_mapFingers["thumb"])->Set_RotZ(30, 45, 20);
+
+	m_mapFingers["index"] = new CFinger;
+	m_mapFingers["index"]->Initialize();
+	dynamic_cast<CFinger*>(m_mapFingers["index"])->Set_Trans({ 400, 600 }, { 340, 495 }, { 340, 405 }, { 340, 330 });
+
+	m_mapFingers["middle"] = new CFinger;
+	m_mapFingers["middle"]->Initialize();
+	dynamic_cast<CFinger*>(m_mapFingers["middle"])->Set_Trans({ 400, 600 }, { 400, 480 }, { 400, 390 }, { 400, 300 });
+
+	m_mapFingers["ring"] = new CFinger;
+	m_mapFingers["ring"]->Initialize();
+	dynamic_cast<CFinger*>(m_mapFingers["ring"])->Set_Trans({ 400, 600 }, { 460, 495 }, { 460, 405 }, { 460, 330 });
+
+	m_mapFingers["little"] = new CFinger;
+	m_mapFingers["little"]->Initialize();
+	dynamic_cast<CFinger*>(m_mapFingers["little"])->Set_Trans({ 400, 600 }, { 505, 510 }, { 520, 465 }, { 520, 395 });
+}
+
+int CFingers::Update()
+{
+	for (auto& Finger : m_mapFingers)
+	{
+		Finger.second->Update();
+	}
+	return 0;
+}
+
+void CFingers::Late_Update()
+{
+	for (auto& Finger : m_mapFingers)
+	{
+		Finger.second->Late_Update();
+
+	}
+	dynamic_cast<CFinger*>(m_mapFingers["thumb"])->Set_Trans({ 400, 600 }, { 300, 510 }, { 285,450 }, { 285,420 });
+	dynamic_cast<CFinger*>(m_mapFingers["thumb"])->Set_RotZ(30, 45, 20);
+}
+
+void CFingers::Render(HDC hDC)
+{
+	for (auto& Finger : m_mapFingers)
+	{
+		Finger.second->Render(hDC);
+	}
+}
+
+void CFingers::Release()
+{
+}
---
+++ DefaultWindow/CJoint.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "CObj.h"
+class CJoint : public CObj
+{
+public:
+	CJoint();
+	virtual ~CJoint();
+
+public:
+	void Set_ParentPtr(CJoint* pParent);
+	void Set_Scale(float _sx, float _sy, float _sz);
+	void Set_RotZ(float _rz);
+	void Set_Trans(float _tx, float _ty, float _tz);
+
+public:
+	void		Initialize() override;
+	int			Update() override;
+	void		Late_Update() override;
+	void		Render(HDC hDC) override;
+	void		Release() override;
+
+private:
+	D3DXVECTOR3		m_vScale;
+	D3DXVECTOR3		m_vRot;
+	D3DXVECTOR3		m_vTrans;
+
+	D3DXVECTOR3     m_vPoint[4];
+	D3DXVECTOR3     m_vOriginPoint[4];
+	CObj*			m_pParent;
+};
+
