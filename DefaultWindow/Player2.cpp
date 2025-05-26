396d1de27e7b8e327a5ae28ff5f49bde02dfbf9b
tballoon23 <asbssu18@gmail.com>
2025-05-26 21:57:41 +0900

    fix : 플레이어 몸체

--- DefaultWindow/Player2.cpp
+++ DefaultWindow/Player2.cpp
@@ -17,12 +17,42 @@ void Player2::Initialize()
 	m_fAngleX = 0.f;
 	m_fAngleY = 0.f;
 	m_fAngleZ = 0.f;
-
 	m_fSpeed = 5.f;
+
 	m_tHead.m_vCenter = m_vCenterHead;
 	m_tHead.m_vScale = { 25.f, 25.f, 0.f };
 	m_tHead.Set_Size(36);
 	m_tHead.SyncToWorld(m_tHead);
+
+	// 몸통 (머리 기준 상대 위치)
+	m_vRelativeBody = { 0.f, 50.f, 0.f };  // 머리 아래 50픽셀
+	m_fAngleBody = 0.f;
+	m_tBody.m_vScale = { 20.f, 40.f, 0.f };
+	m_tBody.Set_Size(4);  // 사각형
+
+	// 왼팔 (머리 기준 상대 위치)
+	m_vRelativeLeftArm = { -30.f, 40.f, 0.f };  // 머리 왼쪽 아래
+	m_fAngleLeftArm = 0.f;
+	m_tLeftArm.m_vScale = { 8.f, 25.f, 0.f };
+	m_tLeftArm.Set_Size(4);
+
+	// 오른팔 (머리 기준 상대 위치)
+	m_vRelativeRightArm = { 30.f, 40.f, 0.f };  // 머리 오른쪽 아래
+	m_fAngleRightArm = 0.f;
+	m_tRightArm.m_vScale = { 8.f, 25.f, 0.f };
+	m_tRightArm.Set_Size(4);
+
+	// 왼다리 (머리 기준 상대 위치)
+	m_vRelativeLeftLeg = { -15.f, 90.f, 0.f };  // 몸통 아래 왼쪽
+	m_fAngleLeftLeg = 0.f;
+	m_tLeftLeg.m_vScale = { 8.f, 30.f, 0.f };
+	m_tLeftLeg.Set_Size(4);
+
+	// 오른다리 (머리 기준 상대 위치)
+	m_vRelativeRightLeg = { 15.f, 90.f, 0.f };  // 몸통 아래 오른쪽
+	m_fAngleRightLeg = 0.f;
+	m_tRightLeg.m_vScale = { 8.f, 30.f, 0.f };
+	m_tRightLeg.Set_Size(4);
 
 }
 
@@ -50,6 +80,41 @@ void Player2::Render(HDC hDC)
 	MoveToEx(hDC, WorldHead.m_vCenter.x, WorldHead.m_vCenter.y + 25, NULL);
 	LineTo(hDC, WorldHead.m_vCenter.x, WorldHead.m_vCenter.y + 75);
 
+	// 몸통 렌더링
+	struct tagPolygon WorldBody;
+	WorldBody.Set_Size(4);
+	for (int i = 0; i < m_tBody.Size(); i++)
+		D3DXVec3TransformCoord(&WorldBody.m_vPoints[i], &m_tBody.m_vPoints[i], &m_matWorldBody);
+	WorldBody.DrawPolygon(hDC);
+
+	// 왼팔 렌더링
+	struct tagPolygon WorldLeftArm;
+	WorldLeftArm.Set_Size(4);
+	for (int i = 0; i < m_tLeftArm.Size(); i++)
+		D3DXVec3TransformCoord(&WorldLeftArm.m_vPoints[i], &m_tLeftArm.m_vPoints[i], &m_matWorldLeftArm);
+	WorldLeftArm.DrawPolygon(hDC);
+
+	// 오른팔 렌더링
+	struct tagPolygon WorldRightArm;
+	WorldRightArm.Set_Size(4);
+	for (int i = 0; i < m_tRightArm.Size(); i++)
+		D3DXVec3TransformCoord(&WorldRightArm.m_vPoints[i], &m_tRightArm.m_vPoints[i], &m_matWorldRightArm);
+	WorldRightArm.DrawPolygon(hDC);
+
+	// 왼다리 렌더링
+	struct tagPolygon WorldLeftLeg;
+	WorldLeftLeg.Set_Size(4);
+	for (int i = 0; i < m_tLeftLeg.Size(); i++)
+		D3DXVec3TransformCoord(&WorldLeftLeg.m_vPoints[i], &m_tLeftLeg.m_vPoints[i], &m_matWorldLeftLeg);
+	WorldLeftLeg.DrawPolygon(hDC);
+
+	// 오른다리 렌더링
+	struct tagPolygon WorldRightLeg;
+	WorldRightLeg.Set_Size(4);
+	for (int i = 0; i < m_tRightLeg.Size(); i++)
+		D3DXVec3TransformCoord(&WorldRightLeg.m_vPoints[i], &m_tRightLeg.m_vPoints[i], &m_matWorldRightLeg);
+	WorldRightLeg.DrawPolygon(hDC);
+
 }
 
 void Player2::Release()
@@ -68,25 +133,43 @@ void Player2::Key_Input()
 
 void Player2::Update_Matrix()
 {
-	D3DXMATRIX	m_matScale;
-	D3DXMATRIX  m_matRotX;
-	D3DXMATRIX  m_matRotY;   // 회전 행렬
-	D3DXMATRIX  m_matRotZ;
-	D3DXMATRIX  m_matTrans;
+	D3DXMATRIX matScale, matRot, matTrans;
 
-	D3DXMatrixScaling(&m_matScale, m_tHead.m_vScale.x, m_tHead.m_vScale.y, m_tHead.m_vScale.z);
-	
-	// 회전 행렬 업데이트
-	D3DXMatrixRotationX(&m_matRotX, m_fAngleX);
-	D3DXMatrixRotationY(&m_matRotY, m_fAngleY);
-	D3DXMatrixRotationZ(&m_matRotZ, m_fAngleZ);
-	
+	// 1. 머리 월드 행렬 (부모)
+	D3DXMatrixScaling(&matScale, m_tHead.m_vScale.x, m_tHead.m_vScale.y, m_tHead.m_vScale.z);
+	D3DXMatrixRotationZ(&matRot, m_fAngleZ);
+	D3DXMatrixTranslation(&matTrans, m_vCenterHead.x, m_vCenterHead.y, m_vCenterHead.z);
+	m_matWorldHead = matScale * matRot * matTrans;
 
-	// 이동 행렬
-	D3DXMatrixTranslation(&m_matTrans, m_vCenterHead.x, m_vCenterHead.y, m_vCenterHead.z);
+	// 2. 몸통 월드 행렬 (머리의 자식)
+	D3DXMatrixScaling(&matScale, m_tBody.m_vScale.x, m_tBody.m_vScale.y, m_tBody.m_vScale.z);
+	D3DXMatrixRotationZ(&matRot, m_fAngleBody);
+	D3DXMatrixTranslation(&matTrans, m_vRelativeBody.x, m_vRelativeBody.y, m_vRelativeBody.z);
+	m_matWorldBody = matScale * matRot * matTrans * m_matWorldHead;
 
-	// 월드 행렬 = (크기 * 회전 * 이동) * 공전 * 부모
-	m_matWorld = m_matScale * m_matRotY * m_matRotX * m_matRotZ * m_matTrans;
+	// 3. 왼팔 월드 행렬 (머리의 자식)
+	D3DXMatrixScaling(&matScale, m_tLeftArm.m_vScale.x, m_tLeftArm.m_vScale.y, m_tLeftArm.m_vScale.z);
+	D3DXMatrixRotationZ(&matRot, m_fAngleLeftArm);
+	D3DXMatrixTranslation(&matTrans, m_vRelativeLeftArm.x, m_vRelativeLeftArm.y, m_vRelativeLeftArm.z);
+	m_matWorldLeftArm = matScale * matRot * matTrans * m_matWorldHead;
+
+	// 4. 오른팔 월드 행렬 (머리의 자식)
+	D3DXMatrixScaling(&matScale, m_tRightArm.m_vScale.x, m_tRightArm.m_vScale.y, m_tRightArm.m_vScale.z);
+	D3DXMatrixRotationZ(&matRot, m_fAngleRightArm);
+	D3DXMatrixTranslation(&matTrans, m_vRelativeRightArm.x, m_vRelativeRightArm.y, m_vRelativeRightArm.z);
+	m_matWorldRightArm = matScale * matRot * matTrans * m_matWorldHead;
+
+	// 5. 왼다리 월드 행렬 (머리의 자식)
+	D3DXMatrixScaling(&matScale, m_tLeftLeg.m_vScale.x, m_tLeftLeg.m_vScale.y, m_tLeftLeg.m_vScale.z);
+	D3DXMatrixRotationZ(&matRot, m_fAngleLeftLeg);
+	D3DXMatrixTranslation(&matTrans, m_vRelativeLeftLeg.x, m_vRelativeLeftLeg.y, m_vRelativeLeftLeg.z);
+	m_matWorldLeftLeg = matScale * matRot * matTrans * m_matWorldHead;
+
+	// 6. 오른다리 월드 행렬 (머리의 자식)
+	D3DXMatrixScaling(&matScale, m_tRightLeg.m_vScale.x, m_tRightLeg.m_vScale.y, m_tRightLeg.m_vScale.z);
+	D3DXMatrixRotationZ(&matRot, m_fAngleRightLeg);
+	D3DXMatrixTranslation(&matTrans, m_vRelativeRightLeg.x, m_vRelativeRightLeg.y, m_vRelativeRightLeg.z);
+	m_matWorldRightLeg = matScale * matRot * matTrans * m_matWorldHead;
 }
 
 void Player2::Update_Direction()
--- DefaultWindow/Player2.h
+++ DefaultWindow/Player2.h
@@ -32,6 +32,35 @@ private:
     bool        m_bHardFlip = false;
 
     D3DXMATRIX  m_matWorld; // 최종 변환 행렬
-    struct tagPolygon m_tHead;
+
+    // 5개 폴리곤
+    tagPolygon m_tHead;        // 머리 (부모)
+    tagPolygon m_tBody;        // 몸통
+    tagPolygon m_tLeftArm;     // 왼팔
+    tagPolygon m_tRightArm;    // 오른팔
+    tagPolygon m_tLeftLeg;     // 왼다리
+    tagPolygon m_tRightLeg;    // 오른다리
+
+    // 5개 월드 행렬
+    D3DXMATRIX m_matWorldHead;      // 머리 월드 행렬 (기준)
+    D3DXMATRIX m_matWorldBody;      // 몸통 월드 행렬
+    D3DXMATRIX m_matWorldLeftArm;   // 왼팔 월드 행렬
+    D3DXMATRIX m_matWorldRightArm;  // 오른팔 월드 행렬
+    D3DXMATRIX m_matWorldLeftLeg;   // 왼다리 월드 행렬
+    D3DXMATRIX m_matWorldRightLeg;  // 오른다리 월드 행렬
+
+    // 자식들의 부모 기준 상대 위치
+    D3DXVECTOR3 m_vRelativeBody;
+    D3DXVECTOR3 m_vRelativeLeftArm;
+    D3DXVECTOR3 m_vRelativeRightArm;
+    D3DXVECTOR3 m_vRelativeLeftLeg;
+    D3DXVECTOR3 m_vRelativeRightLeg;
+
+    // 각 부위별 회전각
+    float m_fAngleBody;
+    float m_fAngleLeftArm;
+    float m_fAngleRightArm;
+    float m_fAngleLeftLeg;
+    float m_fAngleRightLeg;
 };
 
